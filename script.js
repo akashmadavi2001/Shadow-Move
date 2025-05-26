1031248d45af5ec62139e3575d35008ca9d6e33d
Akash Madavi <akashmadavi2001@gmail.com>
2025-05-26 09:13:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let text = document.querySelector('.text');
+let light = document.querySelector('.light');
+
+document.addEventListener('mousemove', function (e) {
+    let lightX = e.clientX;
+    let lightY = e.clientY;
+
+    light.style.top = lightY + "px";
+    light.style.left = lightX + "px";
+
+    let distanceX = lightX - text.offsetLeft - text.offsetWidth / 2;
+    let distanceY = lightY - text.offsetTop - text.offsetHeight / 2;
+
+    let newShadow = '';
+    for (let i = 0; i < 200; i++) {
+        let shadowX = -distanceX * (i / 200);
+        let shadowY = -distanceY * (i / 200);
+        let opacity = 1 - (i / 200);
+
+        newShadow += (newShadow ? ',' : '') + shadowX + 'px ' + shadowY + 'px 0px  rgb(51, 51, 51,' + opacity + ')';
+
+        text.style.textShadow = newShadow;
+
+        console.log(newShadow);
+
+    }
+});
